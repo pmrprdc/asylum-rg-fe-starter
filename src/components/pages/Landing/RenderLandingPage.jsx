e6e5f98a47685649a0fc9c97f7880885b12195c5
Pamir Perdeci <pamirperdeci@gmail.com>
2024-02-06 13:23:45 -0500

    created the bottom sections scaffolding. Added containers, title tags and wrote the stats. To be styled.

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -83,6 +83,34 @@ function RenderLandingPage(props) {
         </div>
       </div>
       <div>
+        <div className="bottom-section">
+          <h2>Systemic Disparity Insights</h2>
+          <div className="bottom-section-stats-container">
+            <div className="bottom-section-stat-box">
+              <h3>36%</h3>
+              <p>
+                By the end of the Trump administration, the average asyluf
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </p>
+            </div>
+            <div className="bottom-section-stat-box">
+              <h3>5%</h3>
+              <p>
+                The New York asylum office grant rate dropped to 5 percent in
+                the fiscal year 2020.
+              </p>
+            </div>
+            <div className="bottom-section-stat-box">
+              <h3>6x Lower</h3>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </p>
+            </div>
+          </div>
+        </div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
         {/* <div className="bottom-section">*/}
         <p onClick={() => scrollToTop()} className="back-to-top">
